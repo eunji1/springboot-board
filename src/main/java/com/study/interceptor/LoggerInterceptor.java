d53d1f5b57a162201fb3d88683af21f1463d0099
eunji1 <sssak1088@gmail.com>
2025-01-25 15:49:25 +0900

    aspect, interceptor

---
+++ src/main/java/com/study/interceptor/LoggerInterceptor.java
@@ -0,0 +1,30 @@
+package com.study.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor {
+
+    // preHandle -> controller -> postHandle
+    // 특정 uri가 호출 됐을 떄 실행
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("===============================================");
+        log.debug("==================== BEGIN ====================");
+        log.debug("Request URI ===> " + request.getRequestURI());
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    // 화면으로 결과 전달
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.debug("==================== END ======================");
+        log.debug("===============================================");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+}
